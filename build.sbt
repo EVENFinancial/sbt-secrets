62d2bbd0dbbb05327d783b8dbe69e61ce1523a53
Kevin Hyland <khy@me.com>
2016-02-05 12:13:58 -0500

    Add KmsUtil - not sure how to test yet

--- build.sbt
+++ build.sbt
@@ -5,5 +5,6 @@ scalaVersion := "2.10.4"
 organization := "com.evenfinancial"
 
 libraryDependencies ++= Seq(
+  "com.amazonaws" % "aws-java-sdk-kms" % "1.10.22",
   "org.scalatest" %% "scalatest" % "2.2.5" % Test
 )
---
+++ src/main/scala/com/evenfinancial/sbt/secrets/KmsUtil.scala
@@ -0,0 +1,19 @@
+package com.evenfinancial.sbt.secrets
+
+import java.nio.ByteBuffer
+import java.util.Base64
+import com.amazonaws.services.kms.AWSKMSClient
+import com.amazonaws.services.kms.model.DecryptRequest
+
+object KmsUtil {
+
+  lazy val client = new AWSKMSClient()
+
+  def decryptDataKey(encryptedDataKey: String): String = {
+    val byteBuffer = ByteBuffer.wrap(Base64.getDecoder.decode(encryptedDataKey.trim))
+    val decryptRequest = new DecryptRequest().withCiphertextBlob(byteBuffer)
+    val decryptResult = client.decrypt(decryptRequest)
+    new String(decryptResult.getPlaintext.array, "utf-8")
+  }
+
+}
