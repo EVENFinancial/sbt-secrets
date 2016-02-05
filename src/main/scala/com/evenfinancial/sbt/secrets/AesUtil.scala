65004c1330e050bc2856f036e23eeb76eb209b72
Kevin Hyland <khy@me.com>
2016-02-05 12:37:28 -0500

    Initial implementation of KmsSecrets

--- src/main/scala/com/evenfinancial/sbt/secrets/AesUtil.scala
+++ src/main/scala/com/evenfinancial/sbt/secrets/AesUtil.scala
@@ -9,8 +9,8 @@ import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
 // @see https://github.com/playframework/playframework/blob/2.4.x/framework/src/play/src/main/scala/play/api/libs/Crypto.scala
 object AesUtil {
 
-  def encrypt(data: String, key: String): String = {
-    val secretKey = buildSecretKey(key)
+  def encrypt(data: String, dataKey: String): String = {
+    val secretKey = buildSecretKey(dataKey)
     val cipher = buildCipher()
     cipher.init(Cipher.ENCRYPT_MODE, secretKey)
     val encrypted = cipher.doFinal(data.getBytes("UTF-8"))
@@ -18,8 +18,8 @@ object AesUtil {
     Base64.getEncoder.encodeToString(result)
   }
 
-  def decrypt(data: String, key: String): String = {
-    val secretKey = buildSecretKey(key)
+  def decrypt(data: String, dataKey: String): String = {
+    val secretKey = buildSecretKey(dataKey)
     val bytes = Base64.getDecoder.decode(data)
     val cipher = buildCipher()
     val iv = bytes.slice(0, cipher.getBlockSize)
@@ -30,10 +30,10 @@ object AesUtil {
 
   private def buildCipher() = Cipher.getInstance("AES/CTR/NoPadding")
 
-  private def buildSecretKey(key: String) = {
+  private def buildSecretKey(dataKey: String) = {
     val algorithm = "AES"
     val messageDigest = MessageDigest.getInstance("SHA-256")
-    messageDigest.update(key.getBytes("utf-8"))
+    messageDigest.update(dataKey.getBytes("utf-8"))
     // max allowed length in bits / (8 bits to a byte)
     val maxAllowedKeyLength = Cipher.getMaxAllowedKeyLength(algorithm) / 8
     val raw = messageDigest.digest().slice(0, maxAllowedKeyLength)
--- src/main/scala/com/evenfinancial/sbt/secrets/KmsSecrets.scala
+++ src/main/scala/com/evenfinancial/sbt/secrets/KmsSecrets.scala
@@ -6,28 +6,53 @@ object KmsSecrets extends AutoPlugin {
 
   override def requires = Secrets
 
+  object autoImport {
+
+    val encryptedKmsDataKeyFile = settingKey[File](
+      "The file containing the encrypted KMS data key."
+    )
+
+    val kmsDataKey = taskKey[String](
+      "The data key used to encrypt / decrypt files."
+    )
+
+  }
+
   import Secrets.autoImport._
+  import autoImport._
 
   override def projectSettings = Seq(
 
+    encryptedKmsDataKeyFile := file("./encrypted-kms-data-key.txt"),
+
+    kmsDataKey := KmsUtil.decryptDataKey(IO.read(encryptedKmsDataKeyFile.value)),
+
     encryptSecretFiles := {
+      val dataKey = kmsDataKey.value
+
       secretFiles.value.map { file =>
-        val encryptedData = IO.read(file)
-        val encryptedFile = file.getParentFile / (file.getName + ".encrypted")
-        IO.write(encryptedFile, encryptedData)
-        encryptedFile
+        val encryptedData = AesUtil.encrypt(IO.read(file), dataKey)
+        val _encryptedFile = encryptedFile(file)
+        IO.write(_encryptedFile, encryptedData)
+        _encryptedFile
       }
     },
 
     decryptSecretFiles := {
+      val dataKey = kmsDataKey.value
+
       secretFiles.value.map { file =>
-        val encryptedFile = file.getParentFile / (file.getName + ".encrypted")
-        val decryptedData = IO.read(encryptedFile)
+        val _encryptedFile = encryptedFile(file)
+        val decryptedData = AesUtil.decrypt(IO.read(_encryptedFile), dataKey)
         IO.write(file, decryptedData)
         file
       }
     }
 
   )
+
+  private def encryptedFile(unencryptedFile: File): File = {
+    unencryptedFile.getParentFile / (unencryptedFile.getName + ".encrypted")
+  }
 
 }
